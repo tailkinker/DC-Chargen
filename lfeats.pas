d806dae3b8577ce7040ad24de076232781efb648
Timothy Groves <rakshasa@rakshasa-virtual-machine.(none)>
2013-11-07 08:28:04 -0500

    Another update to lfeats

--- lfeats.pas
+++ lfeats.pas
@@ -24,434 +24,496 @@ const
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Alert';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Ambidexterity';
       ClassNeed: '';
       LevelNeed: 1;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Arcane Dabbler';
       ClassNeed: '';
       LevelNeed: 6;
       SkillNeed: '';
       AttrNeed: (4, 9);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Blind Fighting';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Cleave';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (1, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Cold Mastery';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Combat Caster';
       ClassNeed: '';
       LevelNeed: 6;
       SkillNeed: '';
       AttrNeed: (5, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Demonic Heritage';
       ClassNeed: '';
       LevelNeed: 6;
       SkillNeed: '';
       AttrNeed: (6, 9);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Diehard';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (3, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Acid)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Cold)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Fire)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Force)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Lightning)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Necrotic)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Psychic)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Energy Resistance (Radiant)';
       ClassNeed: '';
       LevelNeed: 11;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Expert Healer';
       ClassNeed: 'Cleric';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (5, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Fast Dodge';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (2, 11);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Fire Mastery';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Great Cleave';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (3, 15);
-      FeatNeed: 'Improved Cleave'
+      FeatNeed: 'Improved Cleave';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Acid)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Acid)'
+      FeatNeed: 'Energy Resistance (Acid)';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Cold)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Cold) '
+      FeatNeed: 'Energy Resistance (Cold) ';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Fire)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Fire)'
+      FeatNeed: 'Energy Resistance (Fire)';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Force)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Force)'
+      FeatNeed: 'Energy Resistance (Force)';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Lightning)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Lightning)'
+      FeatNeed: 'Energy Resistance (Lightning)';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Necrotic)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Necrotic)'
+      FeatNeed: 'Energy Resistance (Necrotic)';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Psychic)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Psychic)'
+      FeatNeed: 'Energy Resistance (Psychic)';
+      Improves: 0
     ),
     ( FeatName: 'Greater Energy Resistance (Radiant)';
       ClassNeed: '';
       LevelNeed: 21;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: 'Energy Resistance (Radiant)'
+      FeatNeed: 'Energy Resistance (Radiant)';
+      Improves: 0
     ),
     ( FeatName: 'Improved Cleave';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (3, 13);
-      FeatNeed: 'Cleave'
+      FeatNeed: 'Cleave';
+      Improves: 0
     ),
     ( FeatName: 'Improved Critical';
       ClassNeed: '';
       LevelNeed: 16;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Improved Initiative';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Iron Skin';
       ClassNeed: '';
       LevelNeed: 16;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Iron Will';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (5, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Language Training';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (4, 11);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 1
     ),
     ( FeatName: 'Large And In Charge';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (1, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Lay Priest';
       ClassNeed: '';
       LevelNeed: 6;
       SkillNeed: '';
       AttrNeed: (5, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Lightning Mastery';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Lightning Reflexes';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (2, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Lunge';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Mobility';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (2, 13);
-      FeatNeed: 'Fast Dodge'
+      FeatNeed: 'Fast Dodge';
+      Improves: 0
     ),
     ( FeatName: 'Mounted Combat';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: 'Nature';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Necrotic Mastery';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Psychic Mastery';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Quick Draw';
       ClassNeed: '';
       LevelNeed: 6;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Radiant Mastery';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Rapid Reload';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Ritual Caster';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Shot On The Run';
       ClassNeed: '';
       LevelNeed: 6;
       SkillNeed: '';
       AttrNeed: (2, 15);
-      FeatNeed: 'Mobility'
+      FeatNeed: 'Mobility';
+      Improves: 0
     ),
     ( FeatName: 'Spell Mastery';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (0, 0);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Spring Attack';
       ClassNeed: '';
       LevelNeed: 6;
       SkillNeed: '';
       AttrNeed: (2, 15);
-      FeatNeed: 'Mobility'
+      FeatNeed: 'Mobility';
+      Improves: 0
     ),
     ( FeatName: 'Swift';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (2, 11);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Tough As Nails';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (3, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Toughness';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (3, 11);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Two-Weapon Defense';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (2, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Two-Weapon Fighting';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (2, 15);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Weapon Finesse (Dexterity)';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (2, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Weapon Finesse (Constitution)';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (3, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Weapon Finesse (Intelligence)';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (4, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Weapon Finesse (Wisdom)';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (5, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     ),
     ( FeatName: 'Weapon Finesse (Charisma)';
       ClassNeed: '';
       LevelNeed: 0;
       SkillNeed: '';
       AttrNeed: (6, 13);
-      FeatNeed: ''
+      FeatNeed: '';
+      Improves: 0
     )
   );
 
--- perl/feats.pl
+++ perl/feats.pl
@@ -24,13 +24,15 @@ while (<INF>) {
   $req[1] = '0' if ($req[1]=='');
   $req[3] = '0' if ($req[3]=='');
   $req[4] = '0' if ($req[4]=='');
-  
+  $req[6] = '0' if ($req[6]=='');
+    
   print "    ( FeatName: '$name';\n";
   print "      ClassNeed: '$req[0]';\n";
   print "      LevelNeed: $req[1];\n";
   print "      SkillNeed: '$req[2]';\n";
   print "      AttrNeed: ($req[3], $req[4]);\n";  
-  print "      FeatNeed: '$req[5]'\n";
+  print "      FeatNeed: '$req[5]';\n";
+  print "      Improves: $req[6]\n";
   if (eof(INF)) {
     print "    )\n";
   } else {
